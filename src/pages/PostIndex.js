be7c8bf7e4d2c6484d016b638880678f9c0893dd
gedunlap <g.e.dunlap90@gmail.com>
2021-06-27 10:05:22 -0400

    pages and components

---
+++ src/pages/PostIndex.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PostIndex = (props) => {
+    <h1>PostIndex</h1>
+}
+
+export default PostIndex;
---
+++ src/pages/PostShow.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PostShow = (props) => {
+    <h1>PostShow</h1>
+}
+
+export default PostShow;
