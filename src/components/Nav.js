12261503fa5107caa23c1b78b4aa7fc55617681a
gedunlap <g.e.dunlap90@gmail.com>
2021-06-29 11:06:09 -0400

    done style home and about

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = (props) => {
     return (
         <div id="navbar">
             <Link id="navitem" onClick={() => window.location.replace("/#about")}>About</Link>
-            <Link to="/"><img src="https://i.imgur.com/Nuqv0mY.png" alt="" className="navlogo" id="navitem"/></Link>
+            <Link  id="navitem" onClick={() => window.location.replace("/#home")} ><img src="https://i.imgur.com/Nuqv0mY.png" alt="" className="navlogo" /></Link>
             <Link to="/PostIndex" id="navitem">Gallery</Link>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,19 +7,29 @@ import Slideshow from '../components/Slideshow'
 const Home = (props) => {
     return (
         <div className="twelve columns homepage">
-            <div className="herobox">
+            <div className="herobox" id="home">
                 <img src="https://i.imgur.com/Z52ZRUg.png" alt="" style={{height: "90%"}} />
             </div>
             <div className="slidebox">
                 <Slideshow />
             </div>
             <div className="aboutbox">
-                <div className="aboutboxtop">
+                <div className="row">
+                <div className="twelve columns aboutboxtop">
                     <div>XAEGIS</div>
                 </div>
-                <div className="aboutboxbottom"  id="about">
-                    <div className="aboutleft">XAegis is devoted to</div>
-                    <div className="aboutright">We are here so the most devoted and hard working extreme atheletes in the world may get their name out there in order to achieve their goals.</div>
+                </div>
+                
+                <div className="row aboutboxbottom"  id="about">
+                    <div className="aboutleft">
+                        <img className="aboutimg" src="https://i.imgur.com/1QtMi97.png?1" alt="" />
+                        <p className="aboutp">XAegis is here to help you connect with the future of your brand. By posting your clips and information here, you are exposing your skill to thousands of potential sponsorship companies.</p>
+                        <div className="aboutbreak"></div>
+                    </div>
+                    <div className="six columns aboutright">
+                        <p className="aboutp">Every post to Xaegis brings you one step closer to furthing your sports career toward the future you've always dreamed of.</p>
+                        <img className="aboutimg" src="https://i.imgur.com/36iDssd.jpg?1" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
