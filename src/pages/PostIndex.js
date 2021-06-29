6727276f594271883fe43e4ccbd3831678867d4e
gedunlap <g.e.dunlap90@gmail.com>
2021-06-29 14:25:52 -0400

    rename post

--- src/pages/PostIndex.js
+++ src/pages/PostIndex.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Post from '../components/Post'
+import Post from '../components/PostComponent'
 import {Link} from 'react-router-dom'
 
 const PostIndex = (props) => {
